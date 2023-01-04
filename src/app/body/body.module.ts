42883f0793193909457f27349aba3b289a73bcde
zzzMunchizzz <jmllerenaespinoza@gmail.com>
2023-01-04 09:48:46 -0500

    Sesion 10 componentes

---
+++ src/app/body/body.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from './input/input.component';
+import { ListComponent } from './list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    InputComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BodyModule { }
